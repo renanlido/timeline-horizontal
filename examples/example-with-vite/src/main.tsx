4624c7ad2fbdb0ca78020e6d816ae0a48f2e2be2
Renan Oliveira <renanlido@yahoo.com.br>
2024-02-15 13:42:30 -0300

    fix: remove unused code

--- examples/example-with-vite/src/main.tsx
+++ examples/example-with-vite/src/main.tsx
@@ -1,9 +1,8 @@
-import './styles/main.css';
+import './styles/global.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
