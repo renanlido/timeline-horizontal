22c060ccd766ce2658ac66f22f7d87896da98c26
Renan Oliveira <renanlido@yahoo.com.br>
2024-02-16 12:13:21 -0300

    fix: remove unused css

--- lib/src/index.tsx
+++ lib/src/index.tsx
@@ -1,5 +1,3 @@
-import './styles/global.css';
-
 import { ActionButton as Action } from './components/action-button';
 import { Timeline } from './components/timeline';
 import { TimelineEvent as Event } from './components/timeline-event';
